8a84177d705d6eab27bbbdd4ac57de16cf1ab5e6
sk0g <ani.mehta@mail.com>
2019-04-06 09:28:01 +1000

    Check the decision made by users

--- script.sh
+++ script.sh
@@ -4,3 +4,9 @@ echo "This is an Intrusion Detection System"
 echo "Do you want to let this program create files?"
 echo "[Y]es, I want the program to create the files. [N]o, I'll do it myself"
 read decision
+
+case $decision in
+    "Y") echo "Creating files now..." ;;
+    "N") echo "Very well, create the files" ;;
+    *) echo "Invalid input detected. Please try to follow instructions.";;
+esac
