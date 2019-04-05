53af131b0acd7b6033b0fc1a83ae990e67da352d
sk0g <ani.mehta@mail.com>
2019-04-06 09:24:49 +1000

    Add initial prompt

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "This is an Intrusion Detection System"
+echo "Do you want to let this program create files?"
+echo "[Y]es, I want the program to create the files. [N]o, I'll do it myself"
+read decision
