dd1f38cfab9dd3a6fbd88ee6fb4fb9bda3ecb386
sk0g <ani.mehta@mail.com>
2019-04-06 14:59:38 +1000

    createFiles() now creates files and folders

--- script.sh
+++ script.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Magic function does nothing. For now.
+function createFiles() {
+    mkdir "folder1"
+    mkdir "publicfolder"
+    echo "Random data 1" >text1.txt
+    echo "Random data 2." >text2.txt
+    echo "Random data 3..." >text3.txt
+    echo "Random data, but now in a FOLDER" >folder1/text.txt
+    echo "Not so secret data" >publicFolder/text.txt
+}
+
 # Prompt
 echo "This is an Intrusion Detection System"
 echo "Do you want to let this program create files?"
@@ -14,6 +25,7 @@ case $decision in
 "Y")
     echo "Creating files now..."
     createFiles
+    echo "Folders and files have been created."
     ;;
 "N")
     echo "Very well, create the files"
@@ -22,8 +34,3 @@ case $decision in
     echo "Invalid input detected. Please try to follow instructions."
     ;;
 esac
-
-# Magic function does nothing. For now.
-function createFiles() {
-    echo "Just out here, creating files and stuff"
-}
